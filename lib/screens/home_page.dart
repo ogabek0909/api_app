4c34d5ec4a82c4e0e80659b8298c0b0633335def
RustambekSafarov <rustambeksafarovv@gmail.com>
2022-12-06 19:43:56 +0500

    add service and models

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:api_app/services/quiz_api.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,35 +9,62 @@ class HomePage extends StatefulWidget {
 }
 
 int? _groupValue;
+int inx = 0;
 
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Quiz Application'),
+        title: const Text('Quiz Application'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('This is question place'),
-            Expanded(
-              child: ListView.builder(
-                itemCount: 4,
-                itemBuilder: (context, index) {
-                  return RadioListTile(
-                    value: index,
-                    groupValue: _groupValue,
-                    onChanged: (value) {
-                      setState(() {
-                        _groupValue = value;
-                      });
-                    },
+            const Text('This is question place'),
+            FutureBuilder(
+              future: getQuiz(),
+              // initialData: InitialData,
+              builder: (BuildContext context, AsyncSnapshot snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const CircularProgressIndicator();
+                } else if (snapshot.hasData) {
+                  return Expanded(
+                    child: ListView.builder(
+                      itemCount: snapshot.data.length,
+                      itemBuilder: (context, indx) {
+                        return Column(
+                          children: [
+                            Text(snapshot.data![indx]['question']),
+                            Expanded(
+                                child: ListView.builder(
+                              itemBuilder: (context, index) => RadioListTile(
+                                title: snapshot.hasData
+                                    ? (snapshot.data![indx]['options'][indx]
+                                        ['title'])
+                                    : Text('null'),
+                                value: index,
+                                groupValue: _groupValue,
+                                onChanged: (value) {
+                                  setState(() {
+                                    _groupValue = value;
+                                  });
+                                },
+                              ),
+                              itemCount: 2,
+                            ))
+                          ],
+                        );
+                      },
+                    ),
                   );
-                },
-              ),
-            )
+                } else if (snapshot.hasError) {
+                  return const Text('Error');
+                }
+                return const Text('Question');
+              },
+            ),
           ],
         ),
       ),
